93c8119b5a8bc91c4f2e86c4a7d7abd2f7dc8e2e
Hugo Torres Martínez <huguitorres66@gmail.com>
2021-09-27 19:46:02 +0200

    Ejercicio 5 a la mitad

--- times.c
+++ times.c
@@ -11,18 +11,41 @@
 
 #include "times.h"
 #include "sorting.h"
+#include "permutations.c"
+#include <time.h>
+#include <stdlib.h>
+#include <limits.h>
 
 /***************************************************/
 /* Function: average_sorting_time Date:            */
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short average_sorting_time(pfunc_sort metodo, 
-                              int n_perms,
-                              int N, 
-                              PTIME_AA ptime)
-{
-/* Your code */
+short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime){
+  int i, minob = INT_MAX, maxob = INT_MIN, ob, num_ob=0;
+  int** perms = generate_permutations(n_perms, N);
+  clock_t start, end, total;
+  start = clock();
+  for(i=0; i<n_perms; i++){
+    ob = metodo(perms[i], 0, N-1);
+    if(ob<minob){
+      minob = ob;
+    }
+    else if(ob>maxob){
+      maxob = ob;
+    }
+    num_ob+=ob;
+  }
+  end = clock();
+  total = (double)(end-start)/CLOCKS_PER_SEC;
+
+  ptime->max_ob = maxob;
+  ptime->min_ob = minob;
+  ptime->N = N;
+  ptime->n_elems = n_perms;
+  ptime->time = total;
+  ptime->average_ob = num_ob/n_perms;
+  return OK;
 }
 
 /***************************************************/
@@ -30,11 +53,8 @@ short average_sorting_time(pfunc_sort metodo,
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short generate_sorting_times(pfunc_sort method, char* file, 
-                                int num_min, int num_max, 
-                                int incr, int n_perms)
-{
-  /* Your code */
+short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num_max, int incr, int n_perms){
+
 }
 
 /***************************************************/
@@ -42,9 +62,8 @@ short generate_sorting_times(pfunc_sort method, char* file,
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short save_time_table(char* file, PTIME_AA ptime, int n_times)
-{
-  /* your code */
+short save_time_table(char* file, PTIME_AA ptime, int n_times){
+
 }
 
 
