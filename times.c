e8bcf27c1c2c5b1bd75344e4d80c12414d5299a5
hugo <hugitorres66@gmail.com>
2021-10-12 12:08:12 +0200

    avance del 5

--- times.c
+++ times.c
@@ -15,6 +15,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdio.h>
 
 /***************************************************/
 /* Function: average_sorting_time Date:            */
@@ -45,6 +46,11 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
   end = clock();
   total = (double)(end-start)/CLOCKS_PER_SEC;
 
+  for(i=0;i<n_perms;i++){
+    free(perms[i]);
+  }
+  free(perms);
+
   ptime->max_ob = maxob;
   ptime->min_ob = minob;
   ptime->N = N;
@@ -60,7 +66,31 @@ short average_sorting_time(pfunc_sort metodo, int n_perms, int N, PTIME_AA ptime
 /* Your documentation                              */
 /***************************************************/
 short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num_max, int incr, int n_perms){
+  TIME_AA *time=NULL;
+  int i,aux,tam;
 
+  if(!method||!file||num_min<=0||num_max<=0||incr<=0||n_perms<=0)
+    return ERR;
+
+  aux=(num_max-num_min)/incr+1;
+  time=(TIME_AA*)malloc((aux)*sizeof(TIME_AA));
+  if(time==NULL)
+    return ERR;
+
+  for(i=0,tam=num_min;i<aux;i++,tam+=incr){
+    if(average_sorting_time(method,n_perms,tam,&time[i])==ERR){
+      free(time);
+      return ERR;
+    }
+  }
+
+  if(save_time_table(file, time,aux)==ERR){
+    free(time);
+    return ERR;
+  }
+
+  free(time);
+  return OK;
 }
 
 /***************************************************/
@@ -69,7 +99,23 @@ short generate_sorting_times(pfunc_sort method, char* file, int num_min, int num
 /* Your documentation                              */
 /***************************************************/
 short save_time_table(char* file, PTIME_AA ptime, int n_times){
+  FILE *f=NULL;
+  int i;
 
+  if(!file||!ptime)
+    return ERR;
+
+  f=fopen(file,"w");
+  if(f==NULL){
+   return ERR;
+  }
+
+  for(i=0;i<n_times;i++){
+    fprintf(f,"%d | %f | %f | %d | %d\n",ptime[i].N, ptime[i].time, ptime[i].average_ob, ptime[i].max_ob, ptime[i].min_ob);
+  }
+
+  fclose(f);
+  return OK;
 }
 
 
